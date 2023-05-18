6f6807b740005771d51ba5693e8a86687910a318
vateseif <vateseif@gmail.com>
2023-05-18 13:36:39 +0200

    Add simple_world_comm_v2

--- evaluate.py
+++ evaluate.py
@@ -11,7 +11,7 @@ from main import get_env
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('env_name', type=str, default='simple_adversary_v2', help='name of the env',
-                        choices=['simple_adversary_v2', 'simple_spread_v2', 'simple_tag_v2'])
+                        choices=['simple_adversary_v2', 'simple_spread_v2', 'simple_tag_v2', 'simple_world_comm_v2'])
     parser.add_argument('folder', type=str, help='name of the folder where model is saved')
     parser.add_argument('--episode-num', type=int, default=10, help='total episode num during evaluation')
     parser.add_argument('--episode-length', type=int, default=50, help='steps per episode')
